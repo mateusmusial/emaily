ecc9dbb5c0fda18e3652b32114260c4bfe2ef538
Mateus Musial <mateus.musial@gmail.com>
2024-10-19 15:20:20 -0300

    Deployment on heroku

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
-  console.log(req);
   res.send({ message: "Hello world!" });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
