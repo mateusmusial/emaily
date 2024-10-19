44000d27051dbe133fbe93c3973a45cdcc89749d
Mateus Musial <mateus.musial@gmail.com>
2024-10-19 19:36:11 -0300

    Change message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hello world!" });
+  res.send({ message: "Bye world!" });
 });
 
 const PORT = process.env.PORT || 5000;
