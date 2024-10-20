9eb55ba58f92658e111772309e59e4f10fdf8d47
Mateus Musial <mateus.musial@gmail.com>
2024-10-19 22:08:18 -0300

    OAuth callbacks

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ app.get(
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
