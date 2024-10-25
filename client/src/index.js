c5a07ac0ac087ef2cb2a163aa0f962572835a534
Mateus Musial <mateus.musial@gmail.com>
2024-10-24 23:41:38 -0300

    Tailwindcss config

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+import "./index.css";
+
 import App from "./components/App";
 import reducers from "./reducers";
 
