7277d795402812ccbe43d3c8a493c432353f14af
Mateus Musial <mateus.musial@gmail.com>
2024-10-24 22:10:26 -0300

    Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import App from "./components/App";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
