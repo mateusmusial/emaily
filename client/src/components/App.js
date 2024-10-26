8b967c007a6efa235b6257f72a61652fabc172bb
Mateus Musial <mateus.musial@gmail.com>
2024-10-26 15:40:51 -0300

    React-redux connect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,32 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 import Header from "./Header";
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-function App() {
-  return (
-    <div className="max-w-screen-lg mx-auto">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="max-w-screen-lg mx-auto">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
