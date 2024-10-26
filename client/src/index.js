3dcb42bfcb28037fdbc651786373bbc81b9b3062
Mateus Musial <mateus.musial@gmail.com>
2024-10-26 15:34:11 -0300

    Redux Thunk and axios - create actions

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
 
 import "./index.css";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
