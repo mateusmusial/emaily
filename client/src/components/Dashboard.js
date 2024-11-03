344d245e60131794d411360fdc8cd4bed98b556a
Mateus Musial <mateus.musial@gmail.com>
2024-11-03 20:36:01 -0300

    Front-end Survey Creation

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import { FaPlus } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+function Dashboard() {
+  return (
+    <div className="p-4">
+      <div className="">
+        <h1 className="">Dashboard</h1>
+      </div>
+      <div className="fixed bottom-14 right-16 sm:right-20 md:right-24 lg:right-28 xl:right-32 bg-blue-500 hover:bg-blue-700 text-white p-4 rounded-full">
+        <Link className="" to="/surveys/new">
+          <FaPlus className="text-4xl" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
