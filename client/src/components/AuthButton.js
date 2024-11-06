78d86fe87065b57e30e179ab41d8bba9c4b73aff
Mateus Musial <mateus.musial@gmail.com>
2024-11-05 22:47:20 -0300

    Improvements styles AuthButton

--- client/src/components/AuthButton.js
+++ client/src/components/AuthButton.js
@@ -2,9 +2,14 @@ import { FaGoogle } from "react-icons/fa";
 
 function AuthButton(props) {
   return (
-    <div className="text-white bg-red-500 hover:bg-red-700 rounded-lg px-4 py-2 text-center flex flex-row space-x-2">
-      <FaGoogle className="text-xl pt-1" />
-      <a href={props.link}>{props.text}</a>
+    <div className="">
+      <a
+        href={props.link}
+        className="inline-flex items-center space-x-2 text-white bg-red-500 hover:bg-red-700 rounded-lg px-4 py-2 text-center"
+      >
+        <FaGoogle />
+        <span>{props.text}</span>
+      </a>
     </div>
   );
 }
